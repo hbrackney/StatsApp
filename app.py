0bb0e0f0a1860f022079d78b87ef58599407eab7
samslatcher <44731395+samslatcher@users.noreply.github.com>
2024-11-06 12:18:24 -0700

    Python script added to z-test page

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def example_page():
     return render_template("example_page.html")
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=14000)
+    serve(app, host="0.0.0.0", port=2220)
